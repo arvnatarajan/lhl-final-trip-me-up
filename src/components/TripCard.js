359eba3cbd326f9ddfb206a198a4e344e87c3396
bchuup <bchuup@gmail.com>
2016-10-24 15:33:00 +0000

    linked trip card to trip timeline, need to connect to correct timeline

--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -1,26 +1,28 @@
 import React, {PropTypes} from 'react';
 import moment from 'moment';
+import { Link } from 'react-router'
 
 const TripCard = ({ trip, index }) => {
   let tripStart = moment.utc(trip.trip_start).format("DD MMM YYYY");
   let tripEnd = moment.utc(trip.trip_end).format("DD MMM YYYY");
   return (
-    <div className="trip-card" >
-      <div className="trip-card-container">
-        <header className="trip-card-header">
-          <img className="trip-card-img" src={trip.trip_img_url}/>
-        </header>
-        <div className="trip-card-details">
-          <h1>{trip.trip_start_location}</h1>
-          <div>to</div>
-          <h1>{trip.trip_destination}</h1>
-          <div>{tripStart}  -  {tripEnd}</div>
+    <Link to="trip/1">
+      <div className="trip-card" >
+        <div className="trip-card-container">
+          <header className="trip-card-header">
+            <img className="trip-card-img" src={trip.trip_img_url}/>
+          </header>
+          <div className="trip-card-details">
+            <h1>{trip.trip_start_location}</h1>
+            <div>to</div>
+            <h1>{trip.trip_destination}</h1>
+            <div>{tripStart}  -  {tripEnd}</div>
+          </div>
+          <footer className="trip-card-footer">
+          </footer>
         </div>
-        <footer className="trip-card-footer">
-          <p>Click here for trip details</p>
-        </footer>
       </div>
-    </div>
+    </Link>
   )
 }
 

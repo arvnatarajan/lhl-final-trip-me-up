f144c2cfe2b9e965766e97d4d0451e4c952c0179
Arvind Natarajan <Arvind@Arvs-MBP.local>
2016-10-31 16:13:07 -0400

    fixed conflicts

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,21 +3,10 @@ import Login from './Login'
 import Logout from './Logout'
 import { loginUser, logoutUser } from '../actions/index'
 import { Link } from 'react-router'
-import { Navbar, Nav, NavItem} from 'react-bootstrap'
 
 const Navigation = ({ user, isAuthenticated, errorMessage, loginUser, logoutUser, fetchTrips, fetchNotifications }) => (
-
-
-const Navbar = ({ user, isAuthenticated, errorMessage, loginUser, logoutUser, fetchTrips, fetchNotifications }) => (
   <nav className="nav">
     <div className="title">trip me up</div>
-  <nav>
-
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a> Trip me Up </a>
-      </Navbar.Brand>
-    </Navbar.Header>
 
     {!isAuthenticated &&
       <Login
@@ -33,8 +22,6 @@ const Navbar = ({ user, isAuthenticated, errorMessage, loginUser, logoutUser, fe
     }
 
   </nav>
-
-
 )
 
 Navigation.propTypes = {
